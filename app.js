01d0798155571eec0669889e8f2f31485f8232ce
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2025-02-01 01:56:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 
 
 
-app.use(cors({origin: 'https://resolutefront-4e7o.vercel.app', credentials: true,}));
+// app.use(cors({origin: 'https://resolutefront-4e7o.vercel.app', credentials: true,}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
