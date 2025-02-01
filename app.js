b7463b588adc298bf4e18c3034a5ce0cdad78651
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2025-02-01 20:54:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,5 +46,7 @@ app.use('/resolution', resolutionRoutes);
 //   next();
 // });
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.listen(5000, ()=> {console.log("Server is running on port 5000")})
